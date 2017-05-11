0ccce8f614d0fde691eb23ea52db633a5b1a7b8b
vin-droid <vineet.12cs069@abes.ac.in>
2017-05-11 08:44:42 +0530

    add code for post request

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http,Response,Headers,RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
+import {Post} from './post/post';
 
 
 
@@ -9,13 +10,22 @@ import 'rxjs/add/operator/map';
 export class PostService {
   constructor(private http:Http) { }
 
-    getAllPosts(): Observable<any>{
-    return this.http.get('http://localhost:3000/posts.json').map(
-    	res =>{
-    		const data = res.json();
-    		return data;
-    	}
-    )
+  getAllPosts(): Observable<any>{
+  	return this.http.get('http://localhost:3000/posts.json').map(
+  		res =>{
+  			const data = res.json();
+  			return data;
+  		}
+  		)
   }
+
+  createPost(post:Post){
+   var body = JSON.stringify(post);
+   var headers = new Headers();
+   headers.append('Content-Type', 'application/X-www-form-urlencoded');
+  	this.http.post('http://localhost:3000/post',body,{ headers: headers });
+  }
+
+
 
 }
---
+++ src/app/post/post.ts
@@ -0,0 +1,5 @@
+export class Post {
+	constructor(public id?:number,public title?:string,public content?:Text,public update_at?:string,public created_at?:string) {
+		
+	}
+}
